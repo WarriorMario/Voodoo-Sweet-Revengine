fceb0d938867d8f77c2ff3c9aa14b1a0ce18df6f
Raymi Klingers <150462@edu.nhtv.nl>
2017-11-08 22:29:12 +0100

    *[UTILITY] Fixing conventions of the tuple get std functions

--- Engine/VTuple.h
+++ Engine/VTuple.h
@@ -6,10 +6,21 @@ using Tuple = std::tuple<Types...>;
 template<typename T, typename... Types>
 constexpr const T& Get(const Tuple<Types...>& tuple) noexcept
 {
-	return std::get<T>(tuple);
+  return std::get<T>(tuple);
 }
 template<typename T, typename... Types>
 constexpr T& Get(Tuple<Types...>& tuple) noexcept
 {
-	return std::get<T, Types...>(tuple);
+  return std::get<T>(tuple);
+}
+
+template<size_t index, typename... Types>
+constexpr auto Get(const Tuple<Types...>& tuple) noexcept -> const std::tuple_element_t<index, Tuple<Types...>>&
+{
+  return std::get<index>(tuple);
+}
+template<size_t index, typename... Types>
+constexpr auto Get(Tuple<Types...>& tuple) noexcept -> std::tuple_element_t<index, Tuple<Types...>>&
+{
+  return std::get<index>(tuple);
 }
