d4fa5b55f057053e2e62cbc04d3168f680feb7bf
Wallby <maartentenvelden@hotmail.com>
2017-11-10 14:49:34 +0100

    * [UTILITY] Added forward alias fix

--- Engine/VTraits.h
+++ Engine/VTraits.h
@@ -1,15 +1,23 @@
 #pragma once
 #include <type_traits>
 
-template <typename T>
-auto Forward(T&& arg) -> decltype(std::forward(arg))
+template<typename T>
+using RemoveRef = std::remove_reference_t<T>;
+
+template<typename T>
+constexpr T&& Forward(RemoveRef<T>& arg) noexcept
 {
-	return std::forward(arg);
+  return std::forward<T>(arg);
 }
-template <typename T>
-auto Move(T&& arg) -> decltype(std::move(arg))
+template<typename T>
+constexpr T&& Forward(RemoveRef<T>&& arg) noexcept
 {
-	return std::move(arg);
+  return std::forward<T>(arg);
+}
+template<typename T>
+constexpr auto Move(T&& arg) -> decltype(std::move(arg))
+{
+  return std::move(arg);
 }
 
 template<bool Test, typename T = void>
