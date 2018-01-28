a07c318dc70eb5054f6c5a70f9c2a757e76a89c1
Floris Kappeyne <floris.kappeyne@gmail.com>
2018-01-28 15:55:14 +0100

    *[FIX] temp fix

--- Engine/Gameplay/Tile.cpp
+++ Engine/Gameplay/Tile.cpp
@@ -33,7 +33,7 @@ void Tile::Init(int grid_pos_x, int grid_pos_y, int tile_visual,
 
 	// transform the position to screen space
 	pos_x = (grid_pos_x) * SIZE;
-	pos_y = (atlas_height - grid_pos_y) * SIZE;
+	pos_y = (grid_pos_y) * SIZE;
 
 	// the step size in u and v per tile
 	float uv_delta_x = (float)TILE_SIZE_IN_ATLAS / (float)tex_width;
