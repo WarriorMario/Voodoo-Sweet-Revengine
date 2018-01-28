70094e46561b71bb12be72cb599152730f616d49
Floris Kappeyne <floris.kappeyne@gmail.com>
2018-01-28 02:54:40 +0100

    *[ART] added the newest animations *[GAMEPLAY] loaded the new animations, made the player class a bit prettier (spacing)

--- Engine/Gameplay/Player.cpp
+++ Engine/Gameplay/Player.cpp
@@ -3,6 +3,127 @@
 #include "Physics\Physics.h"
 #include "TileGrid.h"
 #include "Tile.h"
+
+// ****************************************************************************
+#define MOVE_LAYER_0 "Images/Kid1_Anim/Layer0/0_Kid1"
+#define MOVE_LAYER_1 "Images/Kid1_Anim/Layer1/1_Kid1"
+#define MOVE_LAYER_2 "Images/Kid1_Anim/Layer2/2_Kid1"
+#define MOVE_LAYER_3 "Images/Kid1_Anim/Layer3/3_Kid1"
+
+#define PLUS_WATER_LAYER_0 "Images/Kid1_Anim/Layer0/0_Kid1W"
+#define PLUS_WATER_LAYER_1 "Images/Kid1_Anim/Layer1/1_Kid1W"
+#define PLUS_WATER_LAYER_2 "Images/Kid1_Anim/Layer2/2_Kid1W"
+#define PLUS_WATER_LAYER_3 "Images/Kid1_Anim/Layer3/3_Kid1W"
+
+#define MIN_WATER_LAYER_0 "Images/Kid1_Anim/Layer0/0_Kid1Hit"
+#define MIN_WATER_LAYER_1 "Images/Kid1_Anim/Layer1/1_Kid1Hit"
+#define MIN_WATER_LAYER_2 "Images/Kid1_Anim/Layer2/2_Kid1Hit"
+#define MIN_WATER_LAYER_3 "Images/Kid1_Anim/Layer3/3_Kid1Hit"
+
+Player::Player(Physics& simulation, TileGrid& grid, int id)
+	:
+	movement(*this, new IdleState),
+	flip_sprite(false),
+	physics_body(simulation.CreateBody(Vec2(40, 30), "Square")),
+	grid(grid),
+	player_id(id),
+	graphics{ 0.1f, 0.1f, 0.1f, 0.1f, 0.1f, 0.1f }
+{
+	graphics[(int)Sprite::Idle].AddLayer(MOVE_LAYER_0, 3);
+	graphics[(int)Sprite::Idle].AddLayer(MOVE_LAYER_1, 3);
+	graphics[(int)Sprite::Idle].AddLayer(MOVE_LAYER_2, 3);
+	graphics[(int)Sprite::Idle].AddLayer(MOVE_LAYER_3, 3);
+
+	graphics[(int)Sprite::Move].AddLayer(MOVE_LAYER_0, 3);
+	graphics[(int)Sprite::Move].AddLayer(MOVE_LAYER_1, 3);
+	graphics[(int)Sprite::Move].AddLayer(MOVE_LAYER_2, 3);
+	graphics[(int)Sprite::Move].AddLayer(MOVE_LAYER_3, 3);
+
+	graphics[(int)Sprite::AddingWater].AddLayer(PLUS_WATER_LAYER_0, 19);
+	graphics[(int)Sprite::AddingWater].AddLayer(PLUS_WATER_LAYER_1, 19);
+	graphics[(int)Sprite::AddingWater].AddLayer(PLUS_WATER_LAYER_2, 19);
+	graphics[(int)Sprite::AddingWater].AddLayer(PLUS_WATER_LAYER_3, 19);
+
+	graphics[(int)Sprite::DumpingWater].AddLayer(MIN_WATER_LAYER_0, 1);
+	graphics[(int)Sprite::DumpingWater].AddLayer(MIN_WATER_LAYER_1, 1);
+	graphics[(int)Sprite::DumpingWater].AddLayer(MIN_WATER_LAYER_2, 1);
+	graphics[(int)Sprite::DumpingWater].AddLayer(MIN_WATER_LAYER_3, 1);
+
+	width = 32.0f;
+	height = 32.0f;
+	x = 400.0f;
+	y = 300.0f;
+	physics_body.body->SetUserData(this);
+	dead = false;
+}
+
+void Player::Update(float dt)
+{
+	if (dead)
+	{
+		return;
+	}
+
+	total_time += dt;
+
+	graphics[(int)Sprite::Idle].ScaleLayer(0, 1.0f);
+	graphics[(int)Sprite::Idle].ScaleLayer(1, 1.0f);
+	graphics[(int)Sprite::Idle].ScaleLayer(2, 1.0f);
+	graphics[(int)Sprite::Idle].ScaleLayer(3, 1.0f);
+	graphics[(int)Sprite::Move].ScaleLayer(0, 1.0f);
+	graphics[(int)Sprite::Move].ScaleLayer(1, 1.0f);
+	graphics[(int)Sprite::Move].ScaleLayer(2, 1.0f);
+	graphics[(int)Sprite::Move].ScaleLayer(3, 1.0f);
+
+	float t = sinf(total_time * 2.0f) / 2.0f + 0.5f;
+	float scale = t + 1.5f;
+	graphics[(int)Sprite::Idle].ScaleLayer(0, scale);
+	graphics[(int)Sprite::Idle].ScaleLayer(1, scale);
+	graphics[(int)Sprite::Idle].ScaleLayer(2, scale);
+	graphics[(int)Sprite::Idle].OffsetLayer(1, Vec2(0, 0 * t));
+	graphics[(int)Sprite::Move].ScaleLayer(0, scale);
+	graphics[(int)Sprite::Move].ScaleLayer(1, scale);
+	graphics[(int)Sprite::Move].ScaleLayer(2, scale);
+	graphics[(int)Sprite::Move].OffsetLayer(1, Vec2(0, 0 * t));
+
+	movement.Update(dt);
+	graphics[(int)curr_sprite].Update(dt);
+
+	physics_body.body->GetWorld()->ClearForces();
+	physics_body.body->SetTransform(Vec2(x, y) / PHYSICS_SCALE, 0);
+}
+
+void Player::Input(::Input & input)
+{
+	movement.Input(input);
+}
+
+bool Player::IsStuck()
+{
+	bool res = false;
+	Vec2 offset = Vec2(width / 2, height / 2);
+
+	// outer corners
+	res |= grid.IsPassable((x + offset.x) / Tile::SIZE, (y + offset.y) / Tile::SIZE) == false;
+	res |= grid.IsPassable((x + offset.x) / Tile::SIZE, (y - offset.y) / Tile::SIZE) == false;
+	res |= grid.IsPassable((x - offset.x) / Tile::SIZE, (y + offset.y) / Tile::SIZE) == false;
+	res |= grid.IsPassable((x - offset.x) / Tile::SIZE, (y - offset.y) / Tile::SIZE) == false;
+
+	// centre points
+	res |= grid.IsPassable((x + offset.x) / Tile::SIZE, y / Tile::SIZE) == false;
+	res |= grid.IsPassable((x - offset.x) / Tile::SIZE, y / Tile::SIZE) == false;
+	res |= grid.IsPassable(x / Tile::SIZE, (y + offset.y) / Tile::SIZE) == false;
+	res |= grid.IsPassable(x / Tile::SIZE, (y - offset.y) / Tile::SIZE) == false;
+
+	return res;
+}
+
+void Player::LoseWater()
+{
+	// Not implemented yet
+	dead = true;
+}
+
 // ****************************************************************************
 void IdleState::OnEnter(Player& player)
 {
@@ -239,11 +360,6 @@ JumpState::State JumpState::Input(::Input& input)
 }
 
 // ****************************************************************************
-#define MOVE_LAYER_0 "Images/God/Layer0/0_Kid1"
-#define MOVE_LAYER_1 "Images/God/Layer1/1_Kid1"
-#define MOVE_LAYER_2 "Images/God/Layer2/2_Kid1"
-#define MOVE_LAYER_3 "Images/God/Layer3/3_Kid1"
-
 void GettingWater::OnEnter(Player& player)
 {
 	Base::OnEnter(player);
@@ -289,100 +405,4 @@ GettingWater::State GettingWater::Input(::Input& input)
 		waterAdding = releaseWaterAmount;
 
 	return nullptr;
-}
-
-// ****************************************************************************
-
-Player::Player(Physics& simulation, TileGrid& grid, int id)
-  :
-  movement(*this, new IdleState),
-  flip_sprite(false),
-  physics_body(simulation.CreateBody(Vec2(40, 30), "Square")),
-  grid(grid),
-  player_id(id),
-  graphics{ 0.1f, 0.1f, 0.1f, 0.1f }
-{
-  graphics[(int)Sprite::Idle].AddLayer(MOVE_LAYER_0, 3);
-  graphics[(int)Sprite::Idle].AddLayer(MOVE_LAYER_1, 3);
-  graphics[(int)Sprite::Idle].AddLayer(MOVE_LAYER_2, 3);
-  graphics[(int)Sprite::Idle].AddLayer(MOVE_LAYER_3, 3);
-
-  graphics[(int)Sprite::Move].AddLayer(MOVE_LAYER_0, 3);
-  graphics[(int)Sprite::Move].AddLayer(MOVE_LAYER_1, 3);
-  graphics[(int)Sprite::Move].AddLayer(MOVE_LAYER_2, 3);
-  graphics[(int)Sprite::Move].AddLayer(MOVE_LAYER_3, 3);
-
-  width = 32.0f;
-  height = 32.0f;
-  x = 400.0f;
-  y = 300.0f;
-  physics_body.body->SetUserData(this);
-  dead = false;
-}
-
-void Player::Update(float dt)
-{
-  if(dead)
-  {
-    return;
-  }
-
-  total_time += dt;
-
-  graphics[(int)Sprite::Idle].ScaleLayer(0, 1.0f);
-  graphics[(int)Sprite::Idle].ScaleLayer(1, 1.0f);
-  graphics[(int)Sprite::Idle].ScaleLayer(2, 1.0f);
-  graphics[(int)Sprite::Idle].ScaleLayer(3, 1.0f);
-  graphics[(int)Sprite::Move].ScaleLayer(0, 1.0f);
-  graphics[(int)Sprite::Move].ScaleLayer(1, 1.0f);
-  graphics[(int)Sprite::Move].ScaleLayer(2, 1.0f);
-  graphics[(int)Sprite::Move].ScaleLayer(3, 1.0f);
-
-  float t = sinf(total_time * 2.0f) / 2.0f + 0.5f;
-  float scale = t + 1.5f;
-  graphics[(int)Sprite::Idle].ScaleLayer(0, scale);
-  graphics[(int)Sprite::Idle].ScaleLayer(1, scale);
-  graphics[(int)Sprite::Idle].ScaleLayer(2, scale);
-  graphics[(int)Sprite::Idle].OffsetLayer(1, Vec2(0, 0 * t));
-  graphics[(int)Sprite::Move].ScaleLayer(0, scale);
-  graphics[(int)Sprite::Move].ScaleLayer(1, scale);
-  graphics[(int)Sprite::Move].ScaleLayer(2, scale);
-  graphics[(int)Sprite::Move].OffsetLayer(1, Vec2(0, 0 * t));
-
-  movement.Update(dt);
-  graphics[(int)curr_sprite].Update(dt);
-
-  physics_body.body->GetWorld()->ClearForces();
-  physics_body.body->SetTransform(Vec2(x, y) / PHYSICS_SCALE, 0);
-}
-
-void Player::Input(::Input & input)
-{
-  movement.Input(input);
-}
-
-bool Player::IsStuck()
-{
-	bool res = false;
-	Vec2 offset = Vec2(width / 2, height / 2);
-
-	// outer corners
-	res |= grid.IsPassable((x + offset.x) / Tile::SIZE, (y + offset.y) / Tile::SIZE) == false;
-	res |= grid.IsPassable((x + offset.x) / Tile::SIZE, (y - offset.y) / Tile::SIZE) == false;
-	res |= grid.IsPassable((x - offset.x) / Tile::SIZE, (y + offset.y) / Tile::SIZE) == false;
-	res |= grid.IsPassable((x - offset.x) / Tile::SIZE, (y - offset.y) / Tile::SIZE) == false;
-
-	// centre points
-	res |= grid.IsPassable((x + offset.x) / Tile::SIZE, y / Tile::SIZE) == false;
-	res |= grid.IsPassable((x - offset.x) / Tile::SIZE, y / Tile::SIZE) == false;
-	res |= grid.IsPassable(x / Tile::SIZE, (y + offset.y) / Tile::SIZE) == false;
-	res |= grid.IsPassable(x / Tile::SIZE, (y - offset.y) / Tile::SIZE) == false;
-
-	return res;
-}
-
-void Player::LoseWater()
-{
-  // Not implemented yet
-  dead = true;
 }
--- Engine/Gameplay/Player.h
+++ Engine/Gameplay/Player.h
@@ -10,170 +10,147 @@
 
 // Should be moved somewhere else
 static const size_t NUM_PLAYERS = 4;
+
 class Physics;
 class TileGrid;
+
 // ****************************************************************************
 class Player
 {
 public:
-  enum Sprite
-  {
-    Idle,
-    Move,
-    Run,
-    Jump,
-  };
+	enum Sprite
+	{
+		Idle,
+		Move,
+		Run,
+		Jump,
+		AddingWater,
+		DumpingWater,
+		NumSprites,
+	};
 
 public:
-  Player(Physics& simulation, TileGrid& grid, int id);
+	Player(Physics& simulation, TileGrid& grid, int id);
 
-  virtual void Update(float dt);
-  void Input(Input& input);
-  void Draw()
-  {
-    b2Vec2 pos = b2Vec2(x, y);
-    b2Vec2 size = b2Vec2(width, height);
-    graphics[(int)curr_sprite].Draw(pos, size, flip_sprite, true);
-  }
+	virtual void Update(float dt);
+	void Input(Input& input);
+	void Draw()
+	{
+		b2Vec2 pos = b2Vec2(x, y);
+		b2Vec2 size = b2Vec2(width, height);
+		graphics[(int)curr_sprite].Draw(pos, size, flip_sprite, true);
+	}
 
-  void SetSprite(Sprite sprite)
-  {
-    curr_sprite = sprite;
-  }
-  void SetFlipped(bool flipped)
-  {
-    flip_sprite = flipped;
-  }
+	void SetSprite(Sprite sprite)
+	{
+		curr_sprite = sprite;
+	}
+	void SetFlipped(bool flipped)
+	{
+		flip_sprite = flipped;
+	}
 
-  bool IsStuck();
-  void LoseWater();
+	bool IsStuck();
+	void LoseWater();
 
 public:
-  float x, y;
-  float width, height;
-  const int player_id;
-  bool is_god;
-  bool dead;
-  Body physics_body;
-  float waterPercentage = 0;
-  float speed = 2.f;
-  float total_time;
+	float x, y;
+	float width, height;
+	const int player_id;
+	bool is_god;
+	bool dead;
+	Body physics_body;
+	float waterPercentage = 0;
+	float speed = 2.f;
+	float total_time;
+
 private:
-  TileGrid & grid;
+	TileGrid & grid;
 
-  StateMachine<Player> movement;
-  LayeredAnimation graphics[4];
+	StateMachine<Player> movement;
+	LayeredAnimation graphics[NumSprites];
 
-  Sprite curr_sprite;
-  bool flip_sprite;
-
+	Sprite curr_sprite;
+	bool flip_sprite;
 };
 
 // ****************************************************************************
+// has the player stand idle
 class IdleState : public IState<Player>
 {
 public:
-  void OnEnter(Player& player) override;
-  void OnExit() override;
+	void OnEnter(Player& player) override;
+	void OnExit() override;
 
-  State Update(float dt) override;
-  State Input(::Input& input) override;
-
+	State Update(float dt) override;
+	State Input(::Input& input) override;
 };
+
+// ****************************************************************************
+// it. moves.
 class MoveState : public IState<Player>
 {
 public:
-  MoveState(b2Vec2 direction)
-    :
-    dir(direction)
-  {}
+	MoveState(b2Vec2 direction)
+		:
+		dir(direction)
+	{}
 
-  void OnEnter(Player& player) override;
-  void OnExit() override;
+	void OnEnter(Player& player) override;
+	void OnExit() override;
 
-  State Update(float dt) override;
-  State Input(::Input& input) override;
+	State Update(float dt) override;
+	State Input(::Input& input) override;
 
 private:
-  b2Vec2 dir;
+	b2Vec2 dir;
 };
 
+// ****************************************************************************
+// faster than the move state
 class RunState : public IState<Player>
 {
 public:
-  RunState(b2Vec2 direction)
-    :
-    direction(direction)
-  {}
+	RunState(b2Vec2 direction)
+		:
+		direction(direction)
+	{}
 
-  void OnEnter(Player& player) override;
-  void OnExit() override;
+	void OnEnter(Player& player) override;
+	void OnExit() override;
 
-  State Update(float dt) override;
-  State Input(::Input& input) override;
+	State Update(float dt) override;
+	State Input(::Input& input) override;
 
 private:
-  Vec2 direction;
-
+	Vec2 direction;
 };
 
 // ****************************************************************************
-class MoveDownSideState : public IState<Player>
-{
-public:
-  MoveDownSideState(Vec2 direction)
-    : direction(direction)
-  {}
-
-  void OnEnter(Player& player) override;
-  void OnExit() override;
-
-  State Update(float dt) override;
-  State Input(::Input& input) override;
-
-private:
-  Vec2 direction;
-
-};
-// ****************************************************************************
-class MoveUpDownState : public IState<Player>
-{
-public:
-  MoveUpDownState(Vec2 direction)
-    : direction(direction)
-  {}
-
-  State Update(float dt) override;
-  State Input(::Input& input) override;
-
-private:
-  b2Vec2 direction;
-};
-
+// pretty much obsolete, but does a nice fart sound nonetheless
 class JumpState : public IState<Player>
 {
 public:
-  JumpState()
-    :
-    direction(0.f),
-    velocity(500.f),
-    jump_y(0.f),
-    fart("fart0.wav")
-  {}
+	JumpState()
+		:
+		direction(0.f),
+		velocity(500.f),
+		jump_y(0.f),
+		fart("fart0.wav")
+	{}
 
-  void OnEnter(Player& player) override;
-  void OnExit() override;
+	void OnEnter(Player& player) override;
+	void OnExit() override;
 
-  State Update(float dt) override;
-  State Input(::Input& input) override;
+	State Update(float dt) override;
+	State Input(::Input& input) override;
 
 private:
-  float base_y;
-  float jump_y;
-  float velocity;
-  float direction;
-  Audio fart;
-
+	float base_y;
+	float jump_y;
+	float velocity;
+	float direction;
+	Audio fart;
 };
 
 // ****************************************************************************
@@ -181,12 +158,12 @@ private:
 class GettingWater : public IState<Player>
 {
 public:
-
 	void OnEnter(Player& player) override;
 	void OnExit() override;
 
 	State Update(float dt) override;
 	State Input(::Input& input) override;
+
 private:
 	//The amount of water that it can consume/release simultaneously
 	float consumingWaterAmount;
